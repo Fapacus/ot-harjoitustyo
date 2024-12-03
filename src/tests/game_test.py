d11afb28ff9ab0ee9318beac3cf2b90396474705
Fapacus <142172371+Fapacus@users.noreply.github.com>
2024-12-03 20:53:18 +0200

    päivitys

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -1,13 +1,13 @@
 import unittest
-import sys
-sys.path.append('C:\\Users\\Fapacus\\Desktop\\Koulu_fileet\\ot-harjoitustyo')
+import sqlite3
 from game import MemoryGame
 
 class TestMemoryGame(unittest.TestCase):
     def setUp(self):
-        self.game = MemoryGame.empty_database()
+        self.game = MemoryGame(sqlite3.connect(":memory:"))
+        self.game.empty_database()
 
     def test_register_user(self):
-        MemoryGame.save_user("username1", "password1")
-        self.assertTrue(MemoryGame.check_user_existence("username1"))
-        self.assertEqual(MemoryGame.check_user_password("username1", "password1"), "password1")
+        self.game.save_user("username1", "password1")
+        self.assertTrue(self.game.check_user_existence("username1"))
+        self.assertEqual(self.game.check_user_password("username1"), "password1")
